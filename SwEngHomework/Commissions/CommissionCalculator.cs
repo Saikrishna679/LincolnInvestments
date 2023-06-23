df9b03686364538b8eb754f6473e34f61a9f8e72
sk191997 <sk-akula@wiu.edu>
2023-06-23 12:03:03 -0400

    LincolnInvestements

---
+++ SwEngHomework/Commissions/CommissionCalculator.cs
@@ -0,0 +1,71 @@
+﻿using Newtonsoft.Json;
+
+namespace SwEngHomework.Commissions
+{
+    public class CommissionCalculator : ICommissionCalculator
+    {
+        private const decimal FeeTier1 = 0.0005m; // 5 bps
+        private const decimal FeeTier2 = 0.0006m; // 6 bps
+        private const decimal FeeTier3 = 0.0007m; // 7 bps
+
+        public IDictionary<string, double> CalculateCommissionsByAdvisor(string jsonInput)
+        {
+            IDictionary<string, double> commissions = new Dictionary<string, double>();
+
+            dynamic data = JsonConvert.DeserializeObject(jsonInput);
+
+            foreach (var advisor in data.advisors)
+            {
+                string name = advisor.name;
+                string level = advisor.level;
+                decimal commissionPercentage = GetCommissionPercentage(level);
+
+                commissions[name] = CalculateCommission(data.accounts, name, commissionPercentage);
+            }
+
+            return commissions;
+        }
+
+        private decimal GetCommissionPercentage(string level)
+        {
+            switch (level)
+            {
+                case "Senior":
+                    return 1.0m; // 100%
+                case "Experienced":
+                    return 0.5m; // 50%
+                case "Junior":
+                    return 0.25m; // 25%
+                default:
+                    throw new ArgumentException("Invalid advisor level.");
+            }
+        }
+
+        private double CalculateCommission(dynamic accounts, string advisorName, decimal commissionPercentage)
+        {
+            decimal commission = 0m;
+
+            foreach (var account in accounts)
+            {
+                if (account.advisor.ToString() == advisorName)
+                {
+                    decimal presentValue = Convert.ToDecimal(account.presentValue.Value);
+                    decimal fee = CalculateAccountFee(presentValue);
+                    commission += fee * commissionPercentage;
+                }
+            }
+
+            return Math.Round((double)commission, 2);
+        }
+
+        private decimal CalculateAccountFee(decimal presentValue)
+        {
+            if (presentValue < 50000m)
+                return presentValue * FeeTier1;
+            else if (presentValue < 100000m)
+                return presentValue * FeeTier2;
+            else
+                return presentValue * FeeTier3;
+        }
+    }
+}
---
+++ SwEngHomework/DescriptiveStatistics/StatsCalculator.cs
@@ -0,0 +1,67 @@
+﻿namespace SwEngHomework.DescriptiveStatistics
+{
+    public class StatsCalculator : IStatsCalculator
+    {
+        public Stats Calculate(string semicolonDelimitedContributions)
+        {
+            Stats stats = new Stats();
+
+            List<double> contributions = ParseContributions(semicolonDelimitedContributions);
+
+            if (contributions.Count == 0)
+                return stats;
+
+            stats.Average = CalculateAverage(contributions);
+            stats.Median = CalculateMedian(contributions);
+            stats.Range = CalculateRange(contributions);
+
+            return stats;
+        }
+
+        private List<double> ParseContributions(string semicolonDelimitedContributions)
+        {
+            List<double> contributions = new List<double>();
+
+            string[] tokens = semicolonDelimitedContributions.Split(';');
+
+            foreach (string token in tokens)
+            {
+                if (double.TryParse(token.Trim().Replace("$", "").Replace(",", ""), out double contribution))
+                {
+                    contributions.Add(contribution);
+                }
+            }
+
+            return contributions;
+        }
+
+        private double CalculateAverage(List<double> contributions)
+        {
+            return Math.Round(contributions.Average(), 2);
+        }
+
+        private double CalculateMedian(List<double> contributions)
+        {
+            List<double> sortedContributions = contributions.OrderBy(c => c).ToList();
+            int count = sortedContributions.Count;
+            int mid = count / 2;
+
+            if (count % 2 == 0)
+            {
+                return Math.Round((sortedContributions[mid - 1] + sortedContributions[mid]) / 2, 2);
+            }
+            else
+            {
+                return Math.Round(sortedContributions[mid], 2);
+            }
+        }
+
+        private double CalculateRange(List<double> contributions)
+        {
+            double max = contributions.Max();
+            double min = contributions.Min();
+            return max - min;
+        }
+
+    }
+}
